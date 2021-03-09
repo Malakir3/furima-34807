0de8df83588133707e87b9070c830fd6489b9616
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-09 21:53:36 +0900

    バリデーション、ストロングパラメーターの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  # ↓これ不要かも。deviseコントローラーは作成不要とのことだから。ついでに, if: :devise_controller?の記述も必要？
-  # before_action :configure_permitted_parameters
+  # ↓これ不要かも。deviseコントローラーは作成不要とのことだから。
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :first_name_kanji, :first_name_kana, :last_name_kanji, :last_name_kana, :birthday])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :first_name_kanji, :first_name_kana, :last_name_kanji, :last_name_kana, :birthday])
+  end
 end
