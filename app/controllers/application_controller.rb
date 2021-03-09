bfde0f230c4dd1d115339ed2bbf162b4a7b6322a
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-09 20:20:10 +0900

    新規登録画面の設定
    - リセットCSSの設定
    - 部分テンプレートの配置
    - ルートパスの設定
    - itemsコントローラーの作成
    - name属性などの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # ↓これ不要かも。deviseコントローラーは作成不要とのことだから。ついでに, if: :devise_controller?の記述も必要？
+  # before_action :configure_permitted_parameters
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :first_name_kanji, :first_name_kana, :last_name_kanji, :last_name_kana, :birthday])
+  # end
 end
