0f7d4a15abd9ac235dc0895973fb60687b2d73ae
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-10 13:51:32 +0900

    新規ユーザー登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  # ↓これ不要かも。deviseコントローラーは作成不要とのことだから。
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :first_name_kanji, :first_name_kana, :last_name_kanji, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :password, :first_name_kanji, :first_name_kana, :last_name_kanji, :last_name_kana, :birthday])
   end
 end
