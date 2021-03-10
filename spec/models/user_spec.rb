e04851a43fef606a3798c9b530f1faa0c3623f3f
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-10 18:49:45 +0900

    factorybot, fakerの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'ユーザー新規登録' do
+    it 'nickname, email, password, password_confirmation, 苗字, 名前, 苗字カナ, 名前カナ, 生年月日があれば登録できる' do
+    end
+
+    it ''
+  
+
+  end
 end
