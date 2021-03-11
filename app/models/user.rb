db0874568c5ef603e337acba3c80c0b69f489581
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-11 15:10:37 +0900

    Response to code review (1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+  
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :first_name_kanji
     validates :last_name_kanji
