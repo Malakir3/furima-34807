c9e117bbe21e920962899cc5ff984befc2978b03
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-12 15:50:34 +0900

    販売手数料、販売利益の計算

---
+++ app/javascript/price.js
@@ -0,0 +1,15 @@
+function price() {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const taxPrice = document.getElementById("add-tax-price");
+    const taxPriceCal = Math.floor(itemPrice.value * 0.1);
+
+    const profit = document.getElementById("profit");
+    const profitCal = itemPrice.value - taxPriceCal;
+
+    taxPrice.innerHTML = taxPriceCal;
+    profit.innerHTML = profitCal;
+  });
+}
+
+window.addEventListener('load', price);
