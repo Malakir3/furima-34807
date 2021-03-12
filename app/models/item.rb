dfa498ee4c86c1694aece4dbf3317ec23d61a7ac
Malakir3 <k.yasuyuki0924@gmail.com>
2021-03-12 11:39:46 +0900

    価格のバリデーション設定
    画像の添付必須も追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,12 @@ class Item < ApplicationRecord
   belongs_to :send_within
   has_one_attached :image
 
-  validates :title, :description, :price, presence: true
-
+  validates :title, :description, :price, :image, presence: true
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :delivery_fee_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :send_within_id, numericality: { other_than: 1 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 end
