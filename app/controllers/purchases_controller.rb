1e175630235e58a8e6826d638eb2ef99deacd3be
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-03-16 18:35:38 +0900

    Formオブジェクトを用いた購入情報の登録機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,32 @@
 class PurchasesController < ApplicationController
   def index
+    set_item
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      set_item
+      render :index
+    end
+  end
+
+  private
+  
+  def purchase_params
+    params.require(:purchase_address).permit(
+      :post_number, :prefecture_id, :city,
+      :house_number, :building_name, :phone_number
+    ).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
 end
