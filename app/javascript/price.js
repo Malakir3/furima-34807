e1fff66c7cb6c49a1130bc30fd9cc5e2f1b9f55c
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-03-19 09:40:56 +0900

    Add preview.js, Update card.js & price.js

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,15 +1,17 @@
-function price() {
-  const itemPrice = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const taxPrice = document.getElementById("add-tax-price");
-    const taxPriceCal = Math.floor(itemPrice.value * 0.1);
+if (document.URL.match(/new/)) {
+  function price() {
+    const itemPrice = document.getElementById("item-price");
+    itemPrice.addEventListener("keyup", () => {
+      const taxPrice = document.getElementById("add-tax-price");
+      const taxPriceCal = Math.floor(itemPrice.value * 0.1);
 
-    const profit = document.getElementById("profit");
-    const profitCal = itemPrice.value - taxPriceCal;
+      const profit = document.getElementById("profit");
+      const profitCal = itemPrice.value - taxPriceCal;
 
-    taxPrice.innerHTML = taxPriceCal;
-    profit.innerHTML = profitCal;
-  });
+      taxPrice.innerHTML = taxPriceCal;
+      profit.innerHTML = profitCal;
+    });
+  }
+
+  window.addEventListener('load', price);
 }
-
-window.addEventListener('load', price);
